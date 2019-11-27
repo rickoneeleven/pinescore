624feaab5571cb3b8f1f2c66de435086f970ec8e
rick111 <r_phpstorm@2012server.local>
2019-11-27 10:07:36 +0000

    want to test production server always returns true

--- application/controllers/partytest.php
+++ application/controllers/partytest.php
@@ -3,7 +3,12 @@
     class Partytest extends CI_Controller {
 
         function index() {
+            $this->load->model('email_dev_or_no');
 
-            echo ENVIRONMENTv2;
+            $array = array(
+                'from_class__method'            => 'bitsNbobs__alertDifference'
+            );
+
+            vdebug($this->email_dev_or_no->amIonAproductionServer($array));
         }
     }
