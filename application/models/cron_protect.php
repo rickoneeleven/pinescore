5197aa4a1401761a5ad702997517d559f4e9f7d5
Ryan Partington <pinescore@outlook.com>
2020-01-22 10:33:02 +0000

    corrected the production server IP

--- application/models/cron_protect.php
+++ application/models/cron_protect.php
@@ -9,7 +9,7 @@ class   Cron_protect extends CI_model {
         $allowedIPs[] = "80.229.138.2"; //home external IP
         $allowedIPs[] = "81.174.163.10"; //vtl plusnet
         $allowedIPs[] = "84.21.150.125"; //vtl Inf
-        $allowedIPs[] = "80.229.138.2"; //novascore's "local IP" - this is where it's requests come from, not 127.
+        $allowedIPs[] = "139.162.250.57"; //novascore's "local IP" - this is where it's requests come from, not 127.
 
         if(in_array($this->input->ip_address(), $allowedIPs)) return TRUE;
         die("who dis new fone?");
