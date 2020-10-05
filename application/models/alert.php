9f2920e0fded19bee2ba028cda99a118ed6c5bb1
rickoneeleven <pinescore@outlook.com>
2020-10-05 16:38:34 +0100

    one for the future

---
+++ application/models/alert.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Alert extends CI_model {
+
+    //array
+    //  'group
+    public function updateMultipleAlertEmailsInGroup($array) {
+
+
+
+    }
+
+
+
+
+}
