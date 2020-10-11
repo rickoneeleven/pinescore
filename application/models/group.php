925dbd2f2652a5b01e17a1dd42a3555dcb8949cd
rickoneeleven <pinescore@outlook.com>
2020-10-11 10:38:55 +0100

    he who take the action, makes the mistakes, that can then be resolved

---
+++ application/models/group.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class   Group extends CI_model {
+
+    public function create() {
+
+    }
+
+    public function read() {
+        
+    }
+
+    public function update() {
+        
+    }
+
+    public function delete() {
+        
+    }
+
+    
+}
