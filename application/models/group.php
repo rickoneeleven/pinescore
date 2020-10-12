7ce7dc03b0c6837641959d45c87a23d06332e79e
rickoneeleven <pinescore@outlook.com>
2020-10-12 08:09:25 +0100

    threw some comment code in to how we're going to build model

--- application/models/group.php
+++ application/models/group.php
@@ -1,19 +1,32 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class   Group extends CI_model {
+class Group extends CI_model {
 
-    public function create() {
+    //'name'
+    //'user'
+    //RETURN group_id
+    public function create($array) {
 
     }
 
-    public function read() {
+    //'group_id'
+    //'user'
+    //RETURN name or FALSE
+    public function read($array) {
         
     }
 
+    //'group_id'
+    //'name'
+    //'user'
+    //RETURN TRUE or FALSE
     public function update() {
         
     }
 
+    //'group_id'
+    //'user'
+    //RETURN TRUE or FALSE
     public function delete() {
         
     }
