b18a3b5fe8cdeab8066eae845f6d553b28c8f65a
Ryan Partington <rick111@Ryans-MacBook-Pro.local>
2020-12-19 09:29:32 +0000

    fix issue with when loading entry page, it was just saying we didn't have access to the group and wouldn't even let us in

--- application/controllers/partytest.php
+++ application/controllers/partytest.php
@@ -1,9 +1,19 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-    class Partytest extends CI_Controller {
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
-        function index() {
-            echo $this->input->ip_address();
-            
+    class Partytest extends CI_Controller
+    {
+        public function index()
+        {
+            echo 'test';
+            echo 'wank';
+            phpinfo();
         }
+
+        //wip1.1 okay xdebug seems to be loaded in phpinfo now, but the breakpoint in partytest isn't stopping
+        //double check your loading partytest from dev server where xdeug is configured, and check vsc xdebug config is
+        //on same server
     }
