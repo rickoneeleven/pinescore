4b85181e4438e89a7b00a213e31a5ab6d200c224
Ryan Partington <pinescore@outlook.com>
2020-12-20 21:42:01 +0000

    wip

--- application/controllers/partytest.php
+++ application/controllers/partytest.php
@@ -9,11 +9,8 @@ if (!defined('BASEPATH')) {
         public function index()
         {
             echo 'test';
-            echo 'wank';
             phpinfo();
         }
 
-        //wip1.1 okay xdebug seems to be loaded in phpinfo now, but the breakpoint in partytest isn't stopping
-        //double check your loading partytest from dev server where xdeug is configured, and check vsc xdebug config is
-        //on same server
+        //wip1 work on editing a group
     }
