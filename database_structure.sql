8cf4e409d426997597532da67a4d3fdc69eff46e
Ryan Partington <pinescore@outlook.com>
2020-12-23 12:11:57 +0000

    updated db structure after group refactor

--- database_structure.sql
+++ database_structure.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+-- MariaDB dump 10.18  Distrib 10.5.8-MariaDB, for osx10.16 (x86_64)
 --
--- Host: 127.0.0.1    Database: pinescore
+-- Host: localhost    Database: pinescore
 -- ------------------------------------------------------
--- Server version	5.7.24
+-- Server version	10.5.8-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `alerts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `alerts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(254) NOT NULL,
@@ -29,17 +29,8 @@ CREATE TABLE `alerts` (
   `unsub_ref` varchar(16) DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=423 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `alerts`
---
-
-LOCK TABLES `alerts` WRITE;
-/*!40000 ALTER TABLE `alerts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `alerts` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `ci_cookies`
@@ -47,7 +38,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `ci_cookies`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ci_cookies` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cookie_id` varchar(255) DEFAULT NULL,
@@ -63,26 +54,17 @@ CREATE TABLE `ci_cookies` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ci_cookies`
---
-
-LOCK TABLES `ci_cookies` WRITE;
-/*!40000 ALTER TABLE `ci_cookies` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ci_cookies` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `ci_sessions`
 --
 
 DROP TABLE IF EXISTS `ci_sessions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ci_sessions` (
   `session_id` varchar(40) NOT NULL DEFAULT '0',
   `ip_address` varchar(45) NOT NULL DEFAULT '0',
   `user_agent` varchar(120) NOT NULL,
-  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `last_activity` int(10) unsigned NOT NULL DEFAULT 0,
   `user_data` text NOT NULL,
   PRIMARY KEY (`session_id`),
   KEY `last_activity_idx` (`last_activity`)
@@ -90,64 +72,69 @@ CREATE TABLE `ci_sessions` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ci_sessions`
---
-
-LOCK TABLES `ci_sessions` WRITE;
-/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `control`
 --
 
 DROP TABLE IF EXISTS `control`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `control` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `datetime` datetime NOT NULL,
   `status` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3723 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `control`
+-- Table structure for table `failed_jobs`
 --
 
-LOCK TABLES `control` WRITE;
-/*!40000 ALTER TABLE `control` DISABLE KEYS */;
-/*!40000 ALTER TABLE `control` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `grouped_reports`
---
-
-DROP TABLE IF EXISTS `grouped_reports`;
+DROP TABLE IF EXISTS `failed_jobs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `grouped_reports` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `name` varchar(16) DEFAULT NULL,
-  `ping_ip_ids` mediumtext,
-  `datetime` datetime DEFAULT NULL,
-  `owner_id` int(5) DEFAULT NULL,
-  `public` tinyint(4) DEFAULT NULL,
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1283 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `grouped_reports`
+-- Table structure for table `group_associations`
 --
 
-LOCK TABLES `grouped_reports` WRITE;
-/*!40000 ALTER TABLE `grouped_reports` DISABLE KEYS */;
-/*!40000 ALTER TABLE `grouped_reports` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `group_associations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group_associations` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `ping_ip_id` smallint(6) NOT NULL,
+  `group_id` smallint(6) NOT NULL,
+  `user_id` smallint(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `user_id` smallint(6) NOT NULL,
+  `public` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `historic_pinescore`
@@ -155,7 +142,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `historic_pinescore`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `historic_pinescore` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `logged` datetime DEFAULT NULL,
@@ -163,17 +150,8 @@ CREATE TABLE `historic_pinescore` (
   `ms` varchar(5) DEFAULT NULL,
   `pinescore` varchar(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=93779 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `historic_pinescore`
---
-
-LOCK TABLES `historic_pinescore` WRITE;
-/*!40000 ALTER TABLE `historic_pinescore` DISABLE KEYS */;
-/*!40000 ALTER TABLE `historic_pinescore` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `history_email_alerts`
@@ -181,7 +159,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `history_email_alerts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `history_email_alerts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner` int(11) NOT NULL,
@@ -189,17 +167,23 @@ CREATE TABLE `history_email_alerts` (
   `datetime` datetime NOT NULL,
   `status` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11710 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `history_email_alerts`
+-- Table structure for table `migrations`
 --
 
-LOCK TABLES `history_email_alerts` WRITE;
-/*!40000 ALTER TABLE `history_email_alerts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `history_email_alerts` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `node_locks`
@@ -207,7 +191,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `node_locks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `node_locks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(70) NOT NULL,
@@ -218,21 +202,12 @@ CREATE TABLE `node_locks` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `node_locks`
---
-
-LOCK TABLES `node_locks` WRITE;
-/*!40000 ALTER TABLE `node_locks` DISABLE KEYS */;
-/*!40000 ALTER TABLE `node_locks` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `other`
 --
 
 DROP TABLE IF EXISTS `other`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `other` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` tinytext NOT NULL,
@@ -242,22 +217,12 @@ CREATE TABLE `other` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `other`
---
-
-LOCK TABLES `other` WRITE;
-/*!40000 ALTER TABLE `other` DISABLE KEYS */;
-INSERT INTO `other` VALUES (1,'percent difference for improving nodes','10'),(2,'ms difference required to highlight on main page. is used on conjuction with percent difference','3'),(3,'hasStatusChanged query time','2020-02-10 11:19:03 | query took 0 seconds | updated to: Offline | IP: 84.21.153.21'),(9,'percent difference for slower nodes to be highlighted','-500');
-/*!40000 ALTER TABLE `other` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `perfmon`
 --
 
 DROP TABLE IF EXISTS `perfmon`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `perfmon` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(25) NOT NULL,
@@ -270,21 +235,12 @@ CREATE TABLE `perfmon` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `perfmon`
---
-
-LOCK TABLES `perfmon` WRITE;
-/*!40000 ALTER TABLE `perfmon` DISABLE KEYS */;
-/*!40000 ALTER TABLE `perfmon` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `ping_ip_table`
 --
 
 DROP TABLE IF EXISTS `ping_ip_table`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ping_ip_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(70) NOT NULL,
@@ -298,21 +254,13 @@ CREATE TABLE `ping_ip_table` (
   `public` int(11) NOT NULL,
   `pinescore` smallint(6) DEFAULT NULL,
   `pinescore_change` datetime DEFAULT NULL,
+  `average_daily_ms` smallint(6) DEFAULT NULL,
   `average_longterm_ms` smallint(6) DEFAULT NULL,
   `lta_difference_algo` smallint(6) DEFAULT NULL,
   `count_direction` varchar(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=612 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ping_ip_table`
---
-
-LOCK TABLES `ping_ip_table` WRITE;
-/*!40000 ALTER TABLE `ping_ip_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ping_ip_table` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `ping_result_table`
@@ -320,7 +268,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `ping_result_table`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ping_result_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(50) NOT NULL,
@@ -331,17 +279,8 @@ CREATE TABLE `ping_result_table` (
   `email_sent` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `datetime` (`datetime`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=44173111 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ping_result_table`
---
-
-LOCK TABLES `ping_result_table` WRITE;
-/*!40000 ALTER TABLE `ping_result_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ping_result_table` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `stats`
@@ -349,7 +288,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `stats`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `stats` (
   `ip` varchar(70) NOT NULL,
   `datetime` datetime NOT NULL,
@@ -358,21 +297,12 @@ CREATE TABLE `stats` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `stats`
---
-
-LOCK TABLES `stats` WRITE;
-/*!40000 ALTER TABLE `stats` DISABLE KEYS */;
-/*!40000 ALTER TABLE `stats` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `stats_total`
 --
 
 DROP TABLE IF EXISTS `stats_total`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `stats_total` (
   `ip` varchar(70) NOT NULL,
   `score` int(11) NOT NULL,
@@ -382,21 +312,12 @@ CREATE TABLE `stats_total` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `stats_total`
---
-
-LOCK TABLES `stats_total` WRITE;
-/*!40000 ALTER TABLE `stats_total` DISABLE KEYS */;
-/*!40000 ALTER TABLE `stats_total` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(50) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
@@ -405,17 +326,8 @@ CREATE TABLE `user` (
   `hideOffline` tinyint(4) NOT NULL,
   `default_EA` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `verify_email`
@@ -423,7 +335,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `verify_email`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `verify_email` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` text NOT NULL,
@@ -431,17 +343,8 @@ CREATE TABLE `verify_email` (
   `datetime` datetime NOT NULL,
   `password` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=59 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `verify_email`
---
-
-LOCK TABLES `verify_email` WRITE;
-/*!40000 ALTER TABLE `verify_email` DISABLE KEYS */;
-/*!40000 ALTER TABLE `verify_email` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -452,4 +355,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-13  7:12:35
+-- Dump completed on 2020-12-23 12:10:51
