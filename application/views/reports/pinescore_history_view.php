aa18fb24d8b35cef6a06391faf159cec50f8a62a
Ryan Partington <pinescore@outlook.com>
2020-12-23 18:06:05 +0000

    spelling correction

--- application/views/reports/pinescore_history_view.php
+++ application/views/reports/pinescore_history_view.php
@@ -1,7 +1,7 @@
 <?php
-$color_nscore = "black";
-$color_ms = "black";
-$link_space = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+$color_nscore = 'black';
+$color_ms = 'black';
+$link_space = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 ?>
 <?php
 echo '<a href="#last">Jump Bottom</a>'.$link_space;
@@ -11,26 +11,36 @@ echo '<a href="#last">Jump Bottom</a>'.$link_space;
 <tr>
     <th width="150px">Node</th>
     <th width="100px">pinescore</th>
-    <th width="100px">Rount Time</th>
+    <th width="100px">Round Time</th>
     <th width="150px">Logged</th>
     <th width="150px">Day of Week</th>
 </tr>
 <?php
-foreach($historic_pinescoreTable->result() as $row) {
-    $color_nscore = "black";
-    $color_ms = "black";
-    if($row->pinescore < 50) $color_nscore = "#664400";
-    if($row->pinescore < 0) $color_nscore = "red";
-    if($row->ms > 100) $color_ms = "#664400";
-    if($row->ms > 1000) $color_ms = "red";
-    if($row->ms == 0) $row->ms = "Offline";
+foreach ($historic_pinescoreTable->result() as $row) {
+    $color_nscore = 'black';
+    $color_ms = 'black';
+    if ($row->pinescore < 50) {
+        $color_nscore = '#664400';
+    }
+    if ($row->pinescore < 0) {
+        $color_nscore = 'red';
+    }
+    if ($row->ms > 100) {
+        $color_ms = '#664400';
+    }
+    if ($row->ms > 1000) {
+        $color_ms = 'red';
+    }
+    if ($row->ms == 0) {
+        $row->ms = 'Offline';
+    }
     echo "<tr>
             <td>$row->ip</td>
             <td><font color=\"$color_nscore\">$row->pinescore</font></td>
             <td><font color=\"$color_ms\">$row->ms</font></td>
             <td>$row->logged</td>
-            <td>".date( "l", strtotime($row->logged))."
-        </tr>";
+            <td>".date('l', strtotime($row->logged)).'
+        </tr>';
 }
 ?>
 </table>
