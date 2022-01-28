d7769741794df6c7fea2699493a14d069a0ec91b
rickoneeleven <pinescore@outlook.com>
2022-01-28 21:33:55 +0000

    started working on adding previous group scores to top of icmptable

---
+++ application/models/group_monthly_scores.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class   Group_monthly_scores extends CI_model {
+    
+    public function get_monthly_scores($group_id) {
+        
+        $this->db->where("group_id", $group_id);
+        $this->db->limit(12);
+        $all = $this->db->get("group_monthly_scores");
+    }
+} 
