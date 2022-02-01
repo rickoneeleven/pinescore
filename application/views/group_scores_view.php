7e7002462a89ed8a01bef57ebe30efac373ac537
rickoneeleven <pinescore@outlook.com>
2022-02-01 12:44:51 +0000

    corrected data view, show it shows the month the group score is for, rather than row create date

--- application/views/group_scores_view.php
+++ application/views/group_scores_view.php
@@ -10,7 +10,7 @@ if(isset($group_id))  {
     //echo "<span style='font-size:25px;'>$groupicon</span>$spaces $spaces";
     foreach ($group_monthly_scores->result() as $row)
     {
-        $newDate = date('M', strtotime($row->created_at));
+        $newDate = date('M', strtotime('-1 day', strtotime($row->created_at)));
         echo $newDate."(".$row->score."), ";
     }
     echo "Today($groupscore)";
