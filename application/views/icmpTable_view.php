97d549388dcc66a51d2212597a58696c626c2e97
rickoneeleven <pinescore@outlook.com>
2022-02-04 16:46:44 +0000

    link2

--- application/views/icmpTable_view.php
+++ application/views/icmpTable_view.php
@@ -19,7 +19,7 @@ if ($this->uri->slash_segment(2) == 'popOut/') {
 echo '
     <td></td>
     <td></td>
-    <td></td>
+    <td><a class="powerful" href="'.base_url().'111/pinscordle.html">[pinescordle]</td>
     <td></td>';
     if ($owner_matches_table) { //last td for darker formatting, only see this column if you're owner
         echo '<td></td>';
