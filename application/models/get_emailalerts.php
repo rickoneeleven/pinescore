ddba74adde2608999b553faa20182f3ba1519fa1
rickoneeleven <pinescore@outlook.com>
2022-09-14 09:30:18 +0100

    finished update to alerts when node is slower or faster than usual. now only sends a max of once a week

--- application/models/get_emailalerts.php
+++ application/models/get_emailalerts.php
@@ -27,4 +27,18 @@ class get_emailalerts extends CI_model {
         $email_addresses = substr($email_addresses, 2);
         return $email_addresses;
     }
+    
+    public function recentAlert($id) {
+        $lastLTAalertDate_older_than_week = "lastLTAalertDate > (NOW() - INTERVAL 1 WEEK)";
+        $this->db->where($lastLTAalertDate_older_than_week);
+        $this->db->where('ping_ip_id', $id);
+        $alertsTable = $this->db->get('alerts');
+        echo "<br>num rows: ".$alertsTable->num_rows()." (id: $id)<br>";
+        if($alertsTable->num_rows() > 0) return TRUE;
+        
+        $update['lastLTAalertDate'] = date('Y:m:d H:i:s');
+        $this->db->where('ping_ip_id', $id);
+        $this->db->update('alerts', $update);
+        return FALSE;
+    }
 }
