34b9eff4a49287ad54b33c1f3cc4d007005206ce
rickoneeleven <pinescore@outlook.com>
2023-02-02 11:28:28 +0000

    group scores were not working correctly after 12 months, now they are

--- application/views/group_scores_view.php
+++ application/views/group_scores_view.php
@@ -1,13 +1,12 @@
 <?php
 echo "</br>";
 if(isset($group_id))  {
-    echo "<strong>$group_name</strong> | Group Scores: "; 
+    echo "<strong>$group_name</strong> | Group Scores: Today($groupscore), "; 
     foreach ($group_monthly_scores->result() as $row)
     {
         $newDate = date('M-y', strtotime('-1 day', strtotime($row->created_at)));
         echo $newDate."(".$row->score."), ";
     }
     if(empty($groupscore)) $groupscore = '<span style="color: red;">.. new group created, group score is still being calulated, please allow an hour</span>';
-    echo "Today($groupscore)";
 }
 ?>
