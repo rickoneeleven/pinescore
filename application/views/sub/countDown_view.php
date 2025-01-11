b831f8f253544c93d6d6faab0dd294c1aaf668bf
rickoneeleven <pinescore@outlook.com>
2025-01-11 21:43:23 +0000

    show how many jobs per min at top of popout view

--- application/views/sub/countDown_view.php
+++ application/views/sub/countDown_view.php
@@ -29,13 +29,17 @@ startTimer(10);  // 60 seconds
 if(!isset($group_id)) $group_id = "";
 $spaces = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
 echo "</br>";
-echo "</br>";
 $week_day_number = date('N');
 $smiles = "";
 while($week_day_number > 0) {
     $smiles = $smiles.":) ";
     $week_day_number--;
 }
+
+echo "<div style='display: flex; justify-content: space-between; align-items: center; width: 100%;'>";
+
+// Left side with existing content
+echo "<div>";
 echo "<strong>Happy ".date('l')." ".$smiles."</strong> $spaces";
 
 if($action == 'refresh') {
@@ -51,4 +55,17 @@ if($action == 'refresh') {
 } else {
     echo '<a href="'.base_url().'tools/popOut/">Resume Auto Refresh</a>';
 }
+echo "</div>";
+
+// Right side with monitoring stats
+echo "<div style='text-align: right; margin-right: 20px;'>";
+echo "<span style='color: " . ($jobs_per_minute < 1000 ? "orange" : "green") . "'>";
+echo "pings/min: " . number_format($jobs_per_minute) . "</span> | ";
+echo "<span style='color: " . ($failed_jobs_past_day == 0 ? "green" : "red") . "'>";
+echo "failed jobs (24h): " . $failed_jobs_past_day . "</span> | ";
+echo "<span style='color: " . ($engine_status == 'active' ? "green" : "red") . "'>";
+echo "engine: " . $engine_status . "</span>";
+echo "</div>";
+
+echo "</div>";
 ?>
